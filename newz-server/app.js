bc81f60ad8ca2b2a1dc44fbd63ec61aa1e8d3077
AmanKamble <amankamble88@gmail.com>
2023-09-29 09:47:22 +0530

    Update app.js

--- newz-server/app.js
+++ newz-server/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
 }))
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
